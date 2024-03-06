642ee5502ff0e16f90ef9dc25be280796ed25c39
iamattri0001 <iamattri0001@gmail.com>
2024-03-06 19:32:47 +0530

    add game board

---
+++ client/src/components/Board.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import toast from "react-hot-toast";
+import { MdClose } from "react-icons/md";
+import { TbOval } from "react-icons/tb";
+const Board = () => {
+  const board = [
+    ["X", null, null],
+    [null, "O", "O"],
+    ["X", null, null],
+  ];
+
+  const sym = "X";
+
+  const handleMove = ({ x, y }) => {
+    toast.success(`${x} ${y}`);
+  };
+
+  const boardIndices = [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [1, 0],
+    [1, 1],
+    [1, 2],
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ];
+
+  return (
+    <div className="grid-cols-3 grid gap-5">
+      {boardIndices.map(([x, y], i) => (
+        <div
+          onClick={() => {
+            if (board[x][y]) {
+              return;
+            }
+            handleMove({ x, y });
+          }}
+          x={x}
+          y={y}
+          key={i}
+          className={`h-16 w-16 ring-2 transition-all rounded flex items-center justify-center ${
+            board[x][y]
+              ? `${
+                  board[x][y] === sym ? `ring-primary-400` : `ring-accent-400`
+                }`
+              : ` ring-primary-300/30 hover:ring-primary-400 cursor-pointer`
+          }`}
+        >
+          {board[x][y] === "X" && (
+            <MdClose
+              className={`${
+                sym === board[x][y] ? `text-primary-300` : `text-accent-300`
+              } text-5xl`}
+            />
+          )}
+          {board[x][y] === "O" && (
+            <TbOval
+              className={`${
+                sym === board[x][y] ? `text-primary-300` : `text-accent-300`
+              } text-5xl`}
+            />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Board;
--- client/src/components/Lobby.jsx
+++ client/src/components/Lobby.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Board from "./Board";
 
 const Lobby = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <Board />
+    </div>
+  );
 };
 
 export default Lobby;
